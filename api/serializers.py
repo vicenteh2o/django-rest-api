1d8eb2f5d217071f2a707d19ecd7eb2805001966
Vicente David Fleitas Mendoza <vicente.fleitas@h2oinnovation.com.py>
2025-08-13 15:29:17 -0300

    feat: json resp and browsable api

--- api/serializers.py
+++ api/serializers.py
@@ -5,7 +5,7 @@ from .models import Product, Order, OrderItem
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ('name', 'description', 'price', 'stock', )
+        fields = ('id', 'name', 'description', 'price', 'stock', )
 
     def validate_price(self, value):
         if value < 0:
