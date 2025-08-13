2548f309f4fe3d5cf71d9274a1fef1313d44e227
Vicente David Fleitas Mendoza <vicente.fleitas@h2oinnovation.com.py>
2025-08-13 16:38:26 -0300

    feat: nested serializer for json resp

--- api/serializers.py
+++ api/serializers.py
@@ -12,3 +12,28 @@ class ProductSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError(
                 "Price must be a positive number.")
         return value
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product_name = serializers.CharField(source='product.name', read_only=True)
+    product_price = serializers.DecimalField(max_digits=10, decimal_places=2,
+                                             source='product.price', read_only=True)
+
+    class Meta:
+        model = OrderItem
+        fields = ('product_name', 'product_price', 'quantity', 'item_subtotal')
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(
+        many=True, read_only=True)
+    total_price = serializers.SerializerMethodField()
+
+    def get_total_price(self, obj):
+        order_items = obj.items.all()
+        return sum(order_item.item_subtotal for order_item in order_items)
+
+    class Meta:
+        model = Order
+        fields = ('order_id', 'user', 'created_at',
+                  'status', 'items', 'total_price')
