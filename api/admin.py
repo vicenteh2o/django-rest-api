0c5c9f66f04d81ca394c1047f633d97836bebd1f
Vicente David Fleitas Mendoza <vicente.fleitas@h2oinnovation.com.py>
2025-08-14 17:05:40 -0300

    feat: add order to admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from api.models import Product, Order, OrderItem
 # Register your models here.
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [
+        OrderItemInline
+    ]
+
+
+admin.site.register(Order, OrderAdmin)
