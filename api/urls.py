2433100a42510e3e2167acfa17bdec8f853632c0
Vicente David Fleitas Mendoza <vicente.fleitas@h2oinnovation.com.py>
2025-08-18 11:00:08 -0300

    feat: add create url for product

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('products/', views.ProductListAPIView.as_view(), name='product_list'),
+    path('products/create/', views.ProductCreateAPIView.as_view(),
+         name='product_create'),
     path('products/info/', views.ProductInfoAPIView.as_view(), name='product_info'),
     path('products/<int:product_id>', views.ProductDetailAPIView.as_view(),
          name='product_detail'),
