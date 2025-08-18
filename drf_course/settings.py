463de4cc6094a03339308f5532a0532e221bd022
Vicente David Fleitas Mendoza <vicente.fleitas@h2oinnovation.com.py>
2025-08-18 11:58:01 -0300

    feat: add jwt auth

--- drf_course/settings.py
+++ drf_course/settings.py
@@ -115,3 +115,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'api.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]}
