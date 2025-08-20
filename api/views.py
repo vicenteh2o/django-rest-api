3c6f2c7de3b7c7dc2577673cf2039ee876695a37
Vicente David Fleitas Mendoza <vicente.fleitas@h2oinnovation.com.py>
2025-08-20 11:04:55 -0300

    feat: use decorators

--- api/views.py
+++ api/views.py
@@ -1,8 +1,7 @@
 from django.db.models import Max
-from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.decorators import action
 from rest_framework import filters, generics, viewsets
-from rest_framework.decorators import api_view
 from rest_framework.pagination import (LimitOffsetPagination,
                                        PageNumberPagination)
 from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
@@ -55,6 +54,12 @@ class OrderViewSet(viewsets.ModelViewSet):
     pagination_class = None
     filterset_class = OrderFilter
     filter_backends = [DjangoFilterBackend]
+
+    @action(detail=False, methods=['get'], url_path='user-orders', permission_classes=[IsAuthenticated])
+    def user_orders(self, request):
+        orders = self.get_queryset().filter(user=request.user)
+        serializer = self.get_serializer(orders, many=True)
+        return Response(serializer.data)
 
 # class OrderListAPIView(generics.ListAPIView):
 #     queryset = Order.objects.prefetch_related('items__product')
